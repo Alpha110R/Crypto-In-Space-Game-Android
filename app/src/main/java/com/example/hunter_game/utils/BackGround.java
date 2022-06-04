d66e25ebc54b0308e02cfb325b485abfc4c0948e
Alon Ronder <68230416+Alpha110R@users.noreply.github.com>
2022-06-04 16:00:41 +0300

    Minor update

--- app/src/main/java/com/example/hunter_game/utils/BackGround.java
+++ app/src/main/java/com/example/hunter_game/utils/BackGround.java
@@ -7,8 +7,7 @@ import com.bumptech.glide.Glide;
 public class BackGround {
     static final String LINK_BACKGROUND="https://images.pexels.com/photos/10257142/pexels-photo-10257142.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
-    public BackGround(){
-    }
+    public BackGround(){ }
     public static void setBackGround(Activity activity, ImageView imageView){
         Glide
                 .with(activity)
